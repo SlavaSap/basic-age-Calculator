059e6802c0e96aa1727b517b6c8608ba20820cf3
SlavaSap <slavik.borsh228@gmail.com>
2026-06-21 02:39:40 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+from math import *
+from unittest import result
+
+age = int(input("enter you age: "))
+age1 = int(input("how much older do you want to become: "))
+result = age + age1
+print(result)
